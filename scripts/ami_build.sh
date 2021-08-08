37d1531576cade8c099cdead8f0ab3d368b78498
Vikas Kumar <vikas@reachvikas.com>
2021-08-08 12:40:57 +1000

    sss

---
+++ scripts/ami_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TARGET_AWS_ACCOUNT=${1}
+TARGET_IAM_ROLE=${2}
+
+ROLE=$(aws sts assume-role --role-arn arn:aws:iam::${TARGET_AWS_ACCOUNT}:role/${TARGET_IAM_ROLE} --role-session-name jenkins-slave)
+export AWS_ACCESS_KEY_ID=$(echo "$ROLE" | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo "$ROLE" | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo "$ROLE" | jq -r .Credentials.SessionToken)
+
+aws sts get-caller-identity
+
+packer validate base_ubuntu.json
+
+packer build base_ubuntu.json
